d0af7e6a943968d206392f10912b294d343527e1
Juliho Castillo <julihocc@users.noreply.github.com>
2024-01-11 17:57:42 -0600

    Add snippet show page with database integration

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,1 +1,27 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
 
+interface SnippetShowPageProps {
+    params: {
+        id: string;
+    }
+}
+
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+
+    const snippet = await db.snippet.findFirst({
+
+        where: {
+            id: parseInt(props.params.id, 10)
+        }
+    });
+
+    if (!snippet) {
+        return notFound();
+    }
+
+    console.log(props);
+    return <div>
+        {snippet.title}
+    </div>
+}
